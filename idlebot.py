032a88f9ca8635b0da005a4a2577cc4e8ff3ce71
Raffael <raffael.diestel@gmail.com>
2017-10-25 20:56:58 +0200

    moved oauth token to config

--- idlebot.py
+++ idlebot.py
@@ -6,7 +6,7 @@ import re
 def main():
     s = socket.socket()
     s.connect(('irc.twitch.tv', 6667))
-    s.send("PASS {}\r\n".format('oauth:mmaxdv87zhrkv4hte5k1tihgiyuhzp').encode('utf-8'))
+    s.send("PASS {}\r\n".format(config.RUHFZY_OAUTH_TOKEN).encode('utf-8'))
     s.send("NICK {}\r\n".format('ruhfzy').encode('utf-8'))
     s.send("JOIN #{}\r\n".format('thearchr').encode('utf-8'))
 
